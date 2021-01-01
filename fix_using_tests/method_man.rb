e3b06dfecf1a8a7a230ea198031fe6d6c3453ce2
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-01 06:43:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,9 @@
 # don't forget to add: require 'pry'
 
-def start_game(string player1, string player2)
+def start_game(String player1, String player2)
   puts "Hello #{player1} & #{player2}"
 end
 
-def play_game(string player1, string player2)
+def play_game(String player1, String player2)
   puts "#{player1} is better than #{player2}"
 end
