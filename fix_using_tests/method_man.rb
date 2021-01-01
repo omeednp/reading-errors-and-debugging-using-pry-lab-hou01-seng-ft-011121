7eb2bfa97fe6bb87b28a3879240d0db4ff4ec840
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-01 06:50:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,13 @@
 # don't forget to add: require 'pry'
 
+require 'pry'
+
 def start_game(player1, player2)
   puts "Hello #{player1} & #{player2}"
+  binding.pry
 end
 
 def play_game(player1, player2)
   puts "#{player1} is better than #{player2}"
 end
+
