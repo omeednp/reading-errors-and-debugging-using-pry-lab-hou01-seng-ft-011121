c73a4f0b64bfd25b8f99c386794fef310995e31c
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-01 06:40:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -8,5 +8,7 @@ def play_game(player1, player2)
   puts "#{player1} is better than #{player2}"
 end
 
-start_game(player1, player2)
-play_game(player1, player2)
+def runner
+  start_game(player1, player2)
+  play_game(player1, player2)
+end
